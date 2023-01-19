655c7682ad853276cf5472851992e48b80cc3b1b
ofe14 <ofeoritseamiteye03@gmail.com>
2023-01-20 00:39:41 +0100

    changes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// This the javascript source code for the calculator which satisfies the arithmetic calculation part of the project
+// in this section buttons,conatainer e.t.c are called from the html file by their id or classname so as to assign an event to them to make them interactive.
+// the addeventlistener function is use to detect interactions from the user, in this case the event is ,the user clicking a button on the calculator and then assign an instruction to be carried out as soon as the event occurs
 let answerboard = document.getElementById("answer")
 let clearbutton = document.getElementById("clear")
 let equalto = document.getElementById("equal")
